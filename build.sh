d2e387274594bbb0db01f195d8028262eb5210d3
krishnaspeace <krishnasaarav@gmailcom>
2024-02-14 11:37:59 +0530

    init build.sh

---
+++ build.sh
@@ -0,0 +1,19 @@
+crave run --no-patch -- "
+
+# Remove local_manifests directory
+rm -rf .repo/localmanifests && /
+
+# Clone local_manifests repository
+git clone https://github.com/krishnaspeace/local_manifests --depth 1 -b main .repo/local_manifests && /
+
+# Sync the repositories
+repo sync -c -j4 --force-sync --no-clone-bundle --no-tags && 
+
+# Set up build environment
+source build/envsetup.sh && 
+
+# Lunch configuration
+lunch lineage_ysl-userdebug
+
+# Build confriguration
+make bacon"
