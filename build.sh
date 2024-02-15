e02e0d00cd811d1fccf6007f1a636c5eaa7c5d9c
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-02-15 11:17:53 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,12 @@
-
 # Remove local_manifests directory
-rm -rf .repo/local_manifests && \
-
+rm -rf .repo/local_manifests
 # Clone local_manifests repository
-git clone https://github.com/krishnaspeace/local_manifests --depth 1 -b main .repo/local_manifests && \
-repo init --depth 1 -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs && \
-# Sync the repositories
-repo sync -c -j4 --force-sync --no-clone-bundle --no-tags &&  \
-
+git clone https://github.com/krishnaspeace/local_manifests --depth 1 -b main .repo/local_manifests
+repo init --depth 1 -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
+repo sync -c -j14 --force-sync --no-clone-bundle --no-tags
 # Set up build environment
-source build/envsetup.sh && \
-
+source build/envsetup.sh
 # Lunch configuration
-lunch lineage_ysl-userdebug && \
-
+lunch lineage_ysl-userdebug
 # Build confriguration
 make bacon
