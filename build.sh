0da24f66d2daa2fd385b4972fa0d9f0916c478a7
krishnaspeace <134842461+krishnaspeace@users.noreply.github.com>
2024-02-15 11:49:41 +0530

    Try to fix fingerprint error

--- build.sh
+++ build.sh
@@ -4,6 +4,10 @@ rm -rf .repo/local_manifests
 git clone https://github.com/krishnaspeace/local_manifests --depth 1 -b main .repo/local_manifests
 repo init --depth 1 -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
 repo sync -c -j14 --force-sync --no-clone-bundle --no-tags
+# removing non working fingerprint
+rm -rf vendor/fingerprint/opensource/interfaces
+# adding working fingerprint
+git clone https://github.com/aneeshsvha/vendor_fingerprint_opensource_interfaces vendor/fingerprint/opensource/interfaces
 # Set up build environment
 source build/envsetup.sh
 # Lunch configuration
