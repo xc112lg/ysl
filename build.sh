c9be903f553ef6f7290717fe33442b39373264ef
krishnaspeace <krishnasaarav@gmailcom>
2024-02-22 17:05:23 +0530

    try to fixing error

--- build.sh
+++ build.sh
@@ -3,7 +3,12 @@ rm -rf .repo/local_manifests
 rm -rf build
 # Clone local_manifests repository
 git clone https://github.com/krishnaspeace/local_manifests --depth 1 -b main .repo/local_manifests
+# Clone crdroid 14
 repo init --depth 1 -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
+# removing failing repo 
+rm -rf prebuilts/clang/host/linux-x86
+rm -rf prebuilts/rust
+# repo sync
 repo sync -c -j14 --force-sync --no-clone-bundle --no-tags --prune
 # removing non working fingerprint
 rm -rf vendor/fingerprint/opensource/interfaces
