298418e26f579e20728e8530f5a28a0d003e8f88
krishnaspeace <krishnasaarav@gmail.com>
2024-02-24 20:50:27 +0530

    trying to fix fingerpring error

--- build.sh
+++ build.sh
@@ -10,6 +10,8 @@ rm -rf prebuilts/clang/host/linux-x86
 rm -rf prebuilts/rust
 # repo sync
 repo sync -c -j14 --force-sync --no-clone-bundle --no-tags --prune
+# removing fingerprint
+rm -rf vendor/ fingerprint/opensource/interfaces
 # Set up build environment
 source build/envsetup.sh
 # Lunch configuration
