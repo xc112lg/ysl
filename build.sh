673ee469abd6c23813d1f09aac3e4a8ce8a56ccc
krishnaspeace <krishnasaarav@gmailcom>
2024-02-26 21:26:43 +0530

    fix fingerprint

--- build.sh
+++ build.sh
@@ -12,13 +12,12 @@ repo init --depth 1 -u https://github.com/crdroidandroid/android.git -b 14.0 --g
 # repo sync
 repo sync -c -j4 --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync
 # fixing fingerprint
-rm -rf vendor/ fingerprint/opensource/interfaces
-git clone https://github.com/aneeshsvha/vendor_fingerprint_opensource_interfaces vendor/fingerprint/opensource/interfaces
+git clone https://github.com/xiaomi-msm8953-devs/android_vendor_fingerprint_opensource_interfaces vendor/fingerprint/opensource/interfaces
 # Set up build environment
 export BUILD_USERNAME=kr
 export BUILD_HOSTNAME=crave
 export CRDROID_MAINTAINER=krishnaspeace
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 source build/envsetup.sh
 # Lunch configuration
 lunch lineage_ysl-userdebug
