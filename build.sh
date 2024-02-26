357cb12a1f58cb255db0d01bc97e97bc4be8f295
krishnaspeace <134842461+krishnaspeace@users.noreply.github.com>
2024-02-26 15:03:51 +0530

    improve build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,26 @@
-# Remove local_manifests directory
-rm -rf .repo/local_manifests
+# Remove directory
+rm -rf .repo/local_manifests/ prebuilts/clang/host/linux-x86
 rm -rf build
+rm -rf prebuilts/rust
 # Clone local_manifests repository
-git clone https://github.com/krishnaspeace/local_manifests --depth 1 -b main .repo/local_manifests.xml
+git clone https://github.com/krishnaspeace/local_manifests.git --depth 1 -b main .repo/local_manifests
+if [ ! 0 == 0 ]
+ then   curl -o .repo/local_manifests https://github.com/krishnaspeace/local_manifests.git
+ fi
 # Clone crdroid 14
 repo init --depth 1 -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
-# removing failing repo 
-rm -rf prebuilts/clang/host/linux-x86
-rm -rf prebuilts/rust
 # repo sync
-repo sync -c -j14 --force-sync --no-clone-bundle --no-tags --prune
+repo sync -c -j4 --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync
 # fixing fingerprint
 rm -rf vendor/ fingerprint/opensource/interfaces
 git clone https://github.com/aneeshsvha/vendor_fingerprint_opensource_interfaces vendor/fingerprint/opensource/interfaces
 # Set up build environment
+export BUILD_USERNAME=kr
+export BUILD_HOSTNAME=crave
+export CRDROID_MAINTAINER=krishnaspeace
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 source build/envsetup.sh
 # Lunch configuration
 lunch lineage_ysl-userdebug
 # Build confriguration
-make bacon
+m bacon
