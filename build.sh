59c62f5e2af0ca6ca0cfd9a691c3410ef7d1d3f3
krishnaspeace <krishnasaarav@gmailcom>
2024-03-05 20:24:49 +0530

    risng

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@ rm -rf .repo/local_manifests/ prebuilts/clang/host/linux-x86
 rm -rf build
 rm -rf prebuilts/rust
 # Clone local_manifests repository
-git clone https://github.com/krishnaspeace/local_manifests.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/krishnaspeace/local_manifests.git --depth 1 -b rising-begonia .repo/local_manifests
 if [ ! 0 == 0 ]
- then   curl -o .repo/local_manifests https://github.com/krishnaspeace/local_manifests.git
+ then   curl -o .repo/local_manifests https://github.com/krishnaspeace/local_manifests/tree/rising-begonia
  fi
-# Clone crdroid 14
-repo init --depth 1 -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
+# Clone rising
+repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
 # repo sync
 repo sync -c -j4 --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync
 # fixing fingerprint
@@ -17,10 +17,10 @@ git clone https://github.com/xiaomi-msm8953-devs/android_vendor_fingerprint_open
 # Set up build environment
 export BUILD_USERNAME=kr
 export BUILD_HOSTNAME=crave
-export CRDROID_MAINTAINER=krishnaspeace
+export RISING_MAINTAINER=krishnaspeace
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 source build/envsetup.sh
 # Lunch configuration
-lunch lineage_ysl-userdebug
+lunch rising_begonia-userdebug
 # Build confriguration
 m bacon
