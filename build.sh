9837fcb892c82588395288b92d85c55e729360c1
krishnaspeace <134842461+krishnaspeace@users.noreply.github.com>
2024-03-10 16:31:14 +0530

    test

--- build.sh
+++ build.sh
@@ -3,16 +3,19 @@ rm -rf .repo/local_manifests
 rm -rf prebuilts/clang/host/linux-x86
 rm -rf build
 rm -rf prebuilts/rust
-# Clone local_manifests repository
-git clone https://github.com/krishnaspeace/local_manifests.git --depth 1 -b rising-begonia .repo/local_manifests
-if [ ! 0 == 0 ]
- then   curl -o .repo/local_manifests https://github.com/krishnaspeace/local_manifests/tree/rising-begonia
- fi
-rm -rf device/redmi/begonia device/mediatek/sepolicy
 # Clone rising
 repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
 # repo sync
 repo sync -c -j4 --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync
+# Cloning trees
+git clone https://github.com/saikrishna1504/device_mediatek_sepolicy -b udc device/mediatek/sepolicy
+git clone https://github.com/saikrishna1504/kernel_xiaomi_mt6785 -b udc kernel/xiaomi/mt6785
+git clone https://github.com/saikrishna1504/vendor_redmi_begonia -b udc vendor/redmi/begonia
+git clone https://github.com/saikrishna1504/vendor_redmi_begonia-firmware -b udc vendor/redmi/begonia-firmware
+git clone https://github.com/saikrishna1504/hardware_mediatek -b udc hardware/mediatek
+git clone https://github.com/krishnaspeace/device_redmi_begonia -b rising device/redmi/begonia
+git clone https://github.com/begonia-dev/android_vendor_redmi_begonia-ims -b 14.0 vendor/redmi/begonia-ims
+git clone https://github.com/begonia-dev/android_packages_apps_RevampedFMRadio -b 14.0 packages/apps/MtkFMRadio
 # Set up build environment
 export BUILD_USERNAME=kr
 export BUILD_HOSTNAME=crave
