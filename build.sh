921b9716fd4cfb614f5905c13491cb63e527e40e
krishnaspeace <134842461+krishnaspeace@users.noreply.github.com>
2024-03-15 14:29:01 +0530

    pe

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ rm -rf .repo/local_manifests
 rm -rf prebuilts/clang/host/linux-x86
 rm -rf build
 rm -rf prebuilts/rust
-# Clone rising
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
+# Clone PixelExperience
+repo init -u https://github.com/PixelExperience/manifest -b fourteen
 # repo sync
 repo sync -c -j4 --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync
 # Cloning trees
@@ -13,22 +13,18 @@ git clone https://github.com/saikrishna1504/kernel_xiaomi_mt6785 -b udc kernel/x
 git clone https://github.com/saikrishna1504/vendor_redmi_begonia -b udc vendor/redmi/begonia
 git clone https://github.com/saikrishna1504/vendor_redmi_begonia-firmware -b udc vendor/redmi/begonia-firmware
 git clone https://github.com/saikrishna1504/hardware_mediatek -b udc hardware/mediatek
-git clone https://github.com/krishnaspeace/device_redmi_begonia -b rising device/redmi/begonia
+git clone https://github.com/krishnaspeace/device_redmi_begonia -b pe device/redmi/begonia
 git clone https://github.com/begonia-dev/android_vendor_redmi_begonia-ims -b 14.0 vendor/redmi/begonia-ims
 git clone https://github.com/begonia-dev/android_packages_apps_RevampedFMRadio -b 14.0 packages/apps/MtkFMRadio
 # Set up build environment
 export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_USERNAME=kr
 export BUILD_HOSTNAME=crave
-export RISING_MAINTAINER=krishnaspeace
+export PIXEL_EXPERIENCE_MAINTAINER=krishnaspeace
 export RISING_PACKAGE_TYPE := AOSP
 source build/envsetup.sh
 opt_patch
 # Lunch configuration
-riseup begonia userdebug
+lunch aosp_begonia_userdebug
 # Build confriguration
-ascend
- echo Date and time: 
- 
-# Print out/build_date.txt
-cat out/build_date.txt
+m bacon
