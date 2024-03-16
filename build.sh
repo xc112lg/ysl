9bbe632e9f395772ff357fb00c51a37a76600e60
krishnaspeace <134842461+krishnaspeace@users.noreply.github.com>
2024-03-16 12:45:13 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,23 +1,22 @@
 # Remove directory
-rm -rf .repo/local_manifests/ prebuilts/clang/host/linux-x86
+rm -rf .repo/* prebuilts/clang/host/linux-x86
 rm -rf build
 rm -rf prebuilts/rust
 # Clone local_manifests repository
 git clone https://github.com/krishnaspeace/local_manifests.git --depth 1 -b evo .repo/local_manifests
 if [ ! 0 == 0 ]
  then   curl -o .repo/local_manifests https://github.com/krishnaspeace/local_manifests.git
- fi
-# Clone Evolution X udc
+# Initialize evo repository
 repo init -u https://github.com/Evolution-X/manifest -b udc
 # repo sync
-repo sync -c -j4 --force-sync --no-clone-bundle --no-tags
+repo sync -c -j4 --force-sync --no-clone-bundle --no-tags --depth 1
 # fixing fingerprint
 rm -rf vendor/fingerprint/opensource/interfaces
 git clone https://github.com/xiaomi-msm8953-devs/android_vendor_fingerprint_opensource_interfaces vendor/fingerprint/opensource/interfaces
 # Set up build environment
 export BUILD_USERNAME=kr
 export BUILD_HOSTNAME=crave
-export CRDROID_MAINTAINER=krishnaspeace
+export EVOLUTION_MAINTAINER=krishnaspeace
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 source build/envsetup.sh
 # Lunch configuration
